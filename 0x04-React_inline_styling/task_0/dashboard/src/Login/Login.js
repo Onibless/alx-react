5077fe5fe086b917040673e99d3f3c205713a720
Onibless <ocheiblessyn@gmail.com>
2024-02-21 23:03:48 +0100

    task

--- 0x04-React_inline_styling/task_0/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/Login/Login.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
-import './Login.css'
+import React from 'react';
+import './Login.css';
 
-export default class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<div className="App">
-					<body className="App-body">
-						<p>Login to access the full dashboard</p>
-						<label htmlFor="email" onClick={() => {
-							// select corresponding input
-							document.getElementById('password').focus();
-						}}>Email</label>
-						<input type="email" id="email" />
-						<label htmlFor="password" onClick={() => {
-							// select corresponding input
-							document.getElementById('password').focus();
-						}}>Password</label>
-						<input type="password" id="password" />
-						<button>OK</button>
-					</body>
-				</div>
-			</React.Fragment>
-		)
-	}
+function Login() {
+  return (
+    <React.Fragment>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+    </React.Fragment>
+  )
 }
+
+export default Login;
--- 0x04-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,30 +1,93 @@
-import React from 'react'
-import close_icon from '../assets/close-icon.png'
-import { getLatestNotification } from '../utils/utils'
+import React from 'react';
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropeTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
 
-import './Notifications.css'
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
 
-export default function Notification(props) {
-  return (
-	<div className="Notifications">
-	  <button style={{ 
-		  position: 'absolute',
-		  background: 'transparent',
-		  border: 'none',
-		  right: '20px',
-		 }} 
-		 aria-label='close' 
-		 onClick={() => {
-		  console.log('Close button has been clicked');
-	  }}>
-	    <img src={close_icon} alt="close" height="15px" width="15px"></img>
-	  </button>
-	  <p>Here is the list of notifications</p>
-	  <ul>
-		  <li data-priority="default">New course available</li>
-		  <li data-priority="urgent">New resume available</li>
-	  	  <li dangerouslySetInnerHTML={getLatestNotification()}></li>
-	  </ul>
-	</div>
-  )
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  render() {
+    return (
+      <>
+        <div className='menuItem'>
+          Your notifications
+        </div>
+        {this.props.displayDrawer? 
+            <div className="Notifications">
+              <button style={{
+                color: '#3a3a3a',
+                fontWeight: 'bold',
+                background: 'none',
+                border: 'none',
+                fontSize: '15px',
+                position: 'absolute',
+                right: '3px',
+                top: '3px',
+                cursor: 'pointer',
+                outline: 'none',
+              }}
+              aria-label="Close"
+              onClick={(e) => {
+                console.log('Close button has been clicked');
+              }}
+              >
+                <img src={closeIcon} alt="close icon" width="15px" />
+              </button>
+              {
+                this.props.listNotifications.length != 0 ?
+                  <p>Here is the list of notifications</p>
+                : null
+              }
+              <ul>
+                {
+                  this.props.listNotifications.length == 0 ?
+                    <NotificationItem type="default" value="No new notification for now" />
+                  : null
+                }
+                {
+                  this.props.listNotifications.map((val, idx)=> {
+                    return <NotificationItem
+                    type={val.type}
+                    value={val.value}
+                    html={val.html}
+                    key={val.id}
+                    markAsRead={this.markAsRead}
+                    id={val.id}
+                  />
+                  })
+                }
+              </ul>
+            </div>
+          :
+            null
+        }
+        
+      </>
+    );
+  }
 }
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: []
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropeTypes.bool,
+  listNotifications: PropeTypes.arrayOf(NotificationItemShape)
+};
+
+export default Notifications;
